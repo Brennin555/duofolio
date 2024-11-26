85a333aafb311d11a19e00ceed3241cc4dec0cf3
Brenno da Rosa <brenno.bor@gmail.com>
2024-11-26 13:56:37 -0300

    adicionando os cards de maneira correta ao serem clicados, faltando apenas relacionar os botoes com os objetos que ainda não foram criados nem adicionados no firebase

--- src/app/componentes/trajetoria/trajetoria.component.ts
+++ src/app/componentes/trajetoria/trajetoria.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./trajetoria.component.css']
 })
 export class TrajetoriaComponent {
+  mostrarCard = false;
+  indiceBotao: number | null = null;
+  botoes = [0, 1, 2, 3, 4];
 
+
+  constructor() { }
+
+  trocarEstadoCard(i: number) {
+    this.indiceBotao = this.indiceBotao === i ? null : i;
+  }
+
+  organizarClasseBotoes(i: number): string {
+    if (true) {
+      switch (i) {
+        case 0:
+          return 'centralizar';
+        case 1:
+          return 'centralizar_l1';
+        case 2:
+          return 'centralizar_l2';
+        case 3:
+          return 'centralizar_l1';
+        case 4:
+          return 'centralizar';
+        default:
+          return 'centralizar';
+      }
+    }else{
+      switch (i) {
+        case 0:
+          return 'centralizar';
+        case 1:
+          return 'centralizar_r1';
+        case 2:
+          return 'centralizar_r2';
+        case 3:
+          return 'centralizar_r1';
+        case 4:
+          return 'centralizar';
+        default:
+          return 'centralizar';
+      }
+    }
+  }
 }
